18beb7baf9d56492112c8f491fbd38c0ae8bdcfa
dejana <dejana.stevanovic@footballerista.com>
2021-05-10 16:21:17 +0200

    add runtime measurement

--- models/run_mspsp_instance.py
+++ models/run_mspsp_instance.py
@@ -1,7 +1,10 @@
 from minizinc import Instance, Model, Solver
+from datetime import datetime
 
 
 def main():
+    # Capture start time
+    startTime = datetime.now()
     # Load mpsp model from file
     mpsp = Model("models\parameter_model.mzn")
     # Add instance file
@@ -12,10 +15,14 @@ def main():
     instance = Instance(chuffed, mpsp)
     # Set full_output to true
     instance["full_output"] = 1
-
+    # Run solver
     result = instance.solve()
+    # Capture end time
+    endTime = datetime.now()
     # Output the result
     print(result)
+    # Output runtime
+    print(endTime - startTime)
 
 
 if __name__ == "__main__": 
